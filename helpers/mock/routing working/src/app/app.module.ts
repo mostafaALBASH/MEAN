929436ac25a080cd717fa6c8d3f6a303f42ebe1a
mostafaALBASH <mostafa_elbesh@outlook.com>
2018-04-15 20:43:14 +0200

    Add files via upload

---
+++ helpers/mock/routing working/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { SandboxComponent } from './components/sandbox/sandbox.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+
+const appRoutes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'about', component:AboutComponent},
+  {path:'user/:id', component:UserDetailsComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SandboxComponent,
+    HomeComponent,
+    AboutComponent,
+    NavbarComponent,
+    UserDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
